c5bb3e48ab5fdf4a68b2a72e5084e62ddf9c6ca7
Ryan Tonini <ryantonini@gmail.com>
2015-08-27 02:31:42 -0400

    added request dependency

--- setup.py
+++ setup.py
@@ -2,16 +2,28 @@ from distutils.core import setup
  
 LONG_DESCRIPTION = open('README.rst').read()
 
+keywords = ["game", "company", "web", "scraper", 
+                "data", "api", "package"]
 setup(
     name='gamefaqs-py',
-    version='0.2.3',
+    version='0.2.4',
     author='Ryan Tonini',
     author_email ='ryantonini@yahoo.com',
     packages=['gamefaqs'],
     url='https://github.com/ryantonini/gamefaqs-py',
-    license=' GNU General Public License v3.0',
+    license='GNU General Public License v3.0',
     description='Retrieve and manage game data from GameFAQS.',
     long_description=LONG_DESCRIPTION,
-    package_data={'gamefaqs': ['data/*.sql']},
-    scripts=['scripts/run_load.py']
+    keywords=keywords,
+    install_requires=['BeautifulSoup==3.2.1', 'requests==2.7.0'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Games/Entertainment",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        "Topic :: Software Development"]
 )
