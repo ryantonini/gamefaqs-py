7b1d38a870ce4b9b467c1ff50a38dbf5f9a4ed45
Ryan Tonini <ryantonini@gmail.com>
2015-09-01 17:40:28 -0400

    added startup scripts for db

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
  
 LONG_DESCRIPTION = open('README.rst').read()
 
-keywords = ["game", "company", "web", "scraper", 
-                "data", "api", "package"]
+keywords = ["game", "company", "cheats", "web", "http", "database", 
+            "scraper", "data", "api", "package"]
 setup(
     name='gamefaqs-py',
-    version='0.2.4',
+    version='0.2.5',
     author='Ryan Tonini',
     author_email ='ryantonini@yahoo.com',
     packages=['gamefaqs'],
@@ -15,6 +15,8 @@ setup(
     description='Retrieve and manage game data from GameFAQS.',
     long_description=LONG_DESCRIPTION,
     keywords=keywords,
+    package_data={'gamefaqs': ['data/gamefaqs.sql']},
+    scripts=['scripts/run_load.py'],
     install_requires=['BeautifulSoup==3.2.1', 'requests==2.7.0'],
     classifiers=[
         "Development Status :: 3 - Alpha",
