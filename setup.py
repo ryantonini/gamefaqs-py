495ad9f5de058d9f9a5392d29528ecd2a81b602d
Ryan Tonini <ryantonini@gmail.com>
2015-09-13 22:07:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ keywords = ["game", "company", "cheats", "web", "http", "database",
             "scraper", "data", "api", "package"]
 setup(
     name='gamefaqs-py',
-    version='0.2.6',
+    version='0.2.7',
     author='Ryan Tonini',
     author_email ='ryantonini@yahoo.com',
     packages=['gamefaqs'],
